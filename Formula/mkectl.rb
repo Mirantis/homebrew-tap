2e548bd2e25ba8dbce63f6656284436055863bc0
Thomas Polkowski <tpolkowski@mirantis.com>
2025-01-28 14:03:11 -0500

    Set version to v4.0.0

--- Formula/mkectl.rb
+++ Formula/mkectl.rb
@@ -3,7 +3,7 @@ require "yaml"
 class Mkectl < Formula
   desc "Tool for creating and managing MKE 4.x kubernetes clusters"
   homepage "https://mirantis.github.io/mke-docs/"
-  VERSION = "v4.0.1-rc.3"
+  VERSION = "v4.0.0"
 
   version VERSION
 
