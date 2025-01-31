9298a5707bd02d2519e22b2ad86bb9e69d493932
Thomas Polkowski <tpolkowski@mirantis.com>
2025-01-31 16:55:46 -0500

    Automate create mkectl bottles

--- Formula/mkectl.rb
+++ Formula/mkectl.rb
@@ -5,6 +5,9 @@ class Mkectl < Formula
 
   version VERSION
 
+  depends_on "k0sproject/tap/k0sctl"
+  depends_on "kubernetes-cli"
+
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/MirantisContainers/mke-release/releases/download/#{VERSION}/mkectl_darwin_x86_64.tar.gz"
     sha256 "67e8d2d14a5b1ad0e1053a0dc39a779d4e5c583792830818afb0d28e696b26c8"
@@ -20,8 +23,8 @@ class Mkectl < Formula
 
   bottle do
     root_url "https://github.com/Mirantis/homebrew-tap/releases/download/mkectl-#{VERSION}"
-    sha256 cellar: :any_skip_relocation, ventura:      "67e8d2d14a5b1ad0e1053a0dc39a779d4e5c583792830818afb0d28e696b26c8"
     sha256 cellar: :any_skip_relocation, arm64_sonoma: "f29bae9d60124e7a197c360f5af36e2186d1539d0c47bdb9f86f03cc4674ccda"
+    sha256 cellar: :any_skip_relocation, ventura:      "67e8d2d14a5b1ad0e1053a0dc39a779d4e5c583792830818afb0d28e696b26c8"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "b967161377040c0906034ba09771b74e9283d6d41d56da46b89ad1d264fa6999"
   end
 
