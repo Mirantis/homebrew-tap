725d6dc5b5137940b03768180d171f85b0a62ef5
tppolkow <16722923+tppolkow@users.noreply.github.com>
2025-02-07 20:58:32 +0000

    Update mkectl homebrew formula to 4.0.0

---
+++ Formula/mkectl@4.0.0.rb
@@ -0,0 +1,38 @@
+class MkectlAT400 < Formula
+  desc "Tool for creating and managing MKE 4.x kubernetes clusters"
+  homepage "https://mirantis.github.io/mke-docs/"
+  VERSION = "4.0.0".freeze
+
+  version VERSION
+
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/MirantisContainers/mke-release/releases/download/v#{VERSION}/mkectl_darwin_x86_64.tar.gz"
+    sha256 "67e8d2d14a5b1ad0e1053a0dc39a779d4e5c583792830818afb0d28e696b26c8"
+  elsif OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/MirantisContainers/mke-release/releases/download/v#{VERSION}/mkectl_darwin_arm64.tar.gz"
+    sha256 "f29bae9d60124e7a197c360f5af36e2186d1539d0c47bdb9f86f03cc4674ccda"
+  elsif OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/MirantisContainers/mke-release/releases/download/v#{VERSION}/mkectl_linux_x86_64.tar.gz"
+    sha256 "b967161377040c0906034ba09771b74e9283d6d41d56da46b89ad1d264fa6999"
+  else
+    odie "Unsupported platform"
+  end
+
+  bottle do
+    root_url "https://github.com/Mirantis/homebrew-tap/releases/download/mkectl-4.0.0"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "2a28e5ef4e067684592704c0fb20a1f94480ce68e7ffcdb10f84754ed82e8577"
+    sha256 cellar: :any_skip_relocation, ventura:      "329643f1e46554b81ed2c62ec2708657502bd166db0353ca1bbe636706520fde"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "d6fa128f908b70f011ebbc4d52425da5452fb655b0105576b697d4ae41852c37"
+  end
+
+  depends_on "kubernetes-cli@1.31"
+
+  def install
+    bin.install "mkectl"
+  end
+
+  test do
+    assert_match "Version: v#{VERSION}", shell_output("#{bin}/mkectl version")
+  end
+end
